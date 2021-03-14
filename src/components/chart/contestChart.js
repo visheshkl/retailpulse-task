64ff27c58926f6cb004f5c420b395493d5664241
visheshkl <visheshkkl@gmail.com>
2021-03-15 00:25:24 +0530

    added status tagline

--- src/components/chart/contestChart.js
+++ src/components/chart/contestChart.js
@@ -1,9 +1,9 @@
-import Chart from './chart';
-import styles from '../../components/dfilter/dfilter.module.css';
-import { useState } from 'react';
-import useMediaQuery from '../../utils/useMediaQuery';
+import Chart from "./chart";
+import styles from "../../components/dfilter/dfilter.module.css";
+import { useState } from "react";
+import useMediaQuery from "../../utils/useMediaQuery";
 
-const buildOptions = (title, scale,display) => {
+const buildOptions = (title, scale, display) => {
   const bodyCSS = parseFloat(
     getComputedStyle(document.querySelector("body"))["font-size"]
   );
@@ -41,47 +41,79 @@ const buildOptions = (title, scale,display) => {
   };
 };
 
-const ContestChart=({cfdata})=>{
-    const [activeFilter,setActiveFilter]=useState("ALL");
-    const isMobile=useMediaQuery("(max-width:800px)");
-    const dataFilter=(o)=>{
-        if(activeFilter==="ALL")
-        return true;
-        return o.phase===activeFilter;
-    }
-    const generateBlockedArray=()=>{
-        return cfdata.filter(dataFilter).map(o=>o.name);
-    }
-    return(
-        <div className={styles.chart}>
-            <div className={styles.butt}>
-                <button className={activeFilter==="ALL"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("ALL")}}>ALL</button>   
-                <button className={activeFilter==="BEFORE"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("BEFORE")}}>BEFORE</button>
-                <button className={activeFilter==="FINISHED"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("FINISHED")}}>FINISHED</button>    
-            </div>
-            <div className={styles.chartcomp}>
-                <Chart
-                type="line"
-                data={{
-                    labels: generateBlockedArray(),
-                    datasets: [
-                    {
-                        data: cfdata.filter(dataFilter).map(o=>{return Number(o.durationSeconds)}),
-                        label: "Contest Duration",
-                        borderColor: " #1ca87d",
-                        fill: false,
-                    },
-                    ],
-                }}
-                options={buildOptions(
-                    `Graph between contest rating and contest duration`,
-                    true,
-                    isMobile?false:true
-                )}
-                />
-            </div>
-        </div>
-    );
-}
+const ContestChart = ({ cfdata }) => {
+  const [activeFilter, setActiveFilter] = useState("ALL");
+  const isMobile = useMediaQuery("(max-width:800px)");
+  const dataFilter = (o) => {
+    if (activeFilter === "ALL") return true;
+    return o.phase === activeFilter;
+  };
+  const generateBlockedArray = () => {
+    return cfdata.filter(dataFilter).map((o) => o.name);
+  };
+  return (
+    <div className={styles.chart}>
+      <div className={styles.butt}>
+        <p className={styles.status}>STATUS</p>
+        <button
+          className={
+            activeFilter === "ALL" ? styles.buttonactive : styles.button
+          }
+          type="button"
+          onClick={() => {
+            setActiveFilter("ALL");
+          }}
+        >
+          ALL
+        </button>
+        <button
+          className={
+            activeFilter === "BEFORE" ? styles.buttonactive : styles.button
+          }
+          type="button"
+          onClick={() => {
+            setActiveFilter("BEFORE");
+          }}
+        >
+          BEFORE
+        </button>
+        <button
+          className={
+            activeFilter === "FINISHED" ? styles.buttonactive : styles.button
+          }
+          type="button"
+          onClick={() => {
+            setActiveFilter("FINISHED");
+          }}
+        >
+          FINISHED
+        </button>
+      </div>
+      <div className={styles.chartcomp}>
+        <Chart
+          type="line"
+          data={{
+            labels: generateBlockedArray(),
+            datasets: [
+              {
+                data: cfdata.filter(dataFilter).map((o) => {
+                  return Number(o.durationSeconds);
+                }),
+                label: "Contest Duration",
+                borderColor: " #1ca87d",
+                fill: false,
+              },
+            ],
+          }}
+          options={buildOptions(
+            `Graph between contest rating and contest duration`,
+            true,
+            isMobile ? false : true
+          )}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default ContestChart;
