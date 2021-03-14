4fc97f7025bab81c1b4f9d18bdd6bfc9c775469d
visheshkl <visheshkkl@gmail.com>
2021-03-14 13:07:04 +0530

    fixed bugs

--- src/components/dfilter/dfilter.js
+++ src/components/dfilter/dfilter.js
@@ -3,10 +3,10 @@ import styles from './dfilter.module.css';
 const Filter=({searchFilter,changeSearchFilter,activeFilter,setActiveFilter,setActive})=>{
     return(<div className={styles.filter}>
         <div className={styles.buttonwrap}>
-            <button className={styles.button} type="button" onClick={()=>{setActiveFilter("ALL"); setActive(1);}}>All</button>
-            <button className={styles.button} type="button" onClick={()=>setActiveFilter("CF")}>CF</button>
-            <button className={styles.button} type="button" onClick={()=>setActiveFilter("ICPC")}>ICPC</button>
-            <button className={styles.button} type="button" onClick={()=>setActiveFilter("FAVORITE")}>Favourites</button>
+            <button className={activeFilter==="ALL"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("ALL"); setActive(1);}}>All</button>
+            <button className={activeFilter==="CF"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("CF"); setActive(1);}}>CF</button>
+            <button className={activeFilter==="ICPC"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("ICPC"); setActive(1);}}>ICPC</button>
+            <button className={activeFilter==="FAVORITE"?styles.buttonactive:styles.button} type="button" onClick={()=>{setActiveFilter("FAVORITE"); setActive(1);}}>Favourites</button>
         </div>
         <div>
             <input className={styles.input1} placeholder="Start typing to search..." type="text" onChange={(e)=>changeSearchFilter(e.target.value)} value={searchFilter}/>
