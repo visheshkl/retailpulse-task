f5e4871dfcb5d6def740bb642a07acc59b331f94
visheshkl <visheshkkl@gmail.com>
2021-03-14 05:53:59 +0530

    updated table

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,9 @@ export default function Home() {
           <Nav/>
           <Filter searchFilter={searchFilter} changeSearchFilter={changeSearchFilter} activeFilter={activeFilter} setActiveFilter={setActiveFilter}/>
           <div className={styles.table}>
-           {cfdata&& <Table cfdata={cfdata.filter(dataFilter).slice((active-1)*pageSize,(active)*pageSize)}/>}
+            {cfdata&& <Table cfdata={cfdata.filter(dataFilter).slice((active-1)*pageSize,(active)*pageSize)}/>}
+            {cfdata&&<Pagination totalCount={cfdata.filter(dataFilter).length/pageSize} active={active} setActive={setActive} pageSize={pageSize} setPageSize={setPageSize}/>}
           </div>
-          {cfdata&&<Pagination totalCount={cfdata.filter(dataFilter).length/pageSize} active={active} setActive={setActive} pageSize={pageSize} setPageSize={setPageSize}/>}
         </div>
   )
 }
